a53444483bcd5a48b53e9ef98946ec98674b511a
Lynda Santiago <you@example.com>
2020-08-21 12:20:06 -0400

    created login form and used axiosWithAuth

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import axiosWithAuth from "./axiosWithAuth";
 
-const Login = () => {
+const Login = (props) => {
+  const [credentials, setCredentials] = useState({
+    username: "Lambda School",
+    password: "i<3Lambd4",
+  });
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const inputHandler = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+  const login = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("./api/login", credentials)
+      .then((res) => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/BubblePage");
+      })
+      .catch((err) =>
+        console.log("error happend wih the post request to API", err)
+      );
+  };
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+
+      <form onSubmit={login}>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          value={credentials.username}
+          onChange={inputHandler}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={credentials.password}
+          onChange={inputHandler}
+        />
+        <button>Login</button>
+      </form>
     </>
   );
 };
--- client/src/components/axiosWithAuth.js
+++ client/src/components/axiosWithAuth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export function axiosWithAuth() {
+export default function axiosWithAuth() {
   const token = localStorage.getItem("token");
   return axios.create({
     baseURL: "http://localhost:5000",
